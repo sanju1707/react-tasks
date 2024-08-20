b292d3761c7147111a009ea8339841484d1c38dd
sanju1707 <princesanju1707@gmail.com>
2024-08-20 12:37:26 +0530

    cssapplied

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,13 @@ import { PrimaryHead } from './components/headings/head'
 import { Image } from './components/images/images'
 import { OrderList } from './components/list/list'
 import { UnorderList } from './components/list/list'
+import { Borders } from './components/borders/borders'
+
 
 function App() {
   return (
       <>
-      <PrimaryHead></PrimaryHead>
-      <Image></Image>
-      <OrderList></OrderList>
-      <UnorderList></UnorderList>
+      <Borders></Borders>
       </>
   )
 }
---
+++ src/components/borders/borders.jsx
@@ -0,0 +1,13 @@
+
+import './borders.css'
+
+export const Borders=()=>{
+    return(
+        <div className="maindiv">
+            <div className='div1'></div>
+            <div className='div1'></div>
+            <div className='div1'></div>
+            <div className='div1' style={{backgroundColor:"pink",borderRadius:'16px'}}></div>
+        </div>
+    )
+}
