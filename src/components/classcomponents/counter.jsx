7c89e08877096bc91f272c589f114df9295f14f9
sanju1707 <princesanju1707@gmail.com>
2024-08-25 10:58:16 +0530

    claacommits

---
+++ src/components/classcomponents/counter.jsx
@@ -0,0 +1,8 @@
+import { Component } from "react";
+
+
+class Counter extends Component{
+    render(){
+        
+    }
+}
--- src/components/classcomponents/youtubeclass.jsx
+++ src/components/classcomponents/youtubeclass.jsx
@@ -1,11 +1,21 @@
 import { Component } from "react";
 
-
+import "./youtube.css"
 
 class YoutubeClass extends Component{
+    state={
+        isSub:false,
+        text1:"subscribe",
+        text2:"subscribed"
+    }
+    clickHandler=()=>{
+        this.setState({
+            isSub:!this.state.isSub
+        },()=>{})
+    }
     render(){
         return(
-            <button>subcribe</button>
+            <button className={this.state.isSub?"youtube1":"youtube"} onClick={this.clickHandler}>{this.state.isSub?this.state.text2:this.state.text1}</button>
         )
     }
 }
