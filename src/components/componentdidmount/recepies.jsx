edcd66afe4a36a1a63a3f433a2fcaa6242852f7c
sanju1707 <princesanju1707@gmail.com>
2024-08-28 16:40:35 +0530

    scrap1

--- src/components/componentdidmount/recepies.jsx
+++ src/components/componentdidmount/recepies.jsx
@@ -1,65 +1,60 @@
+import { Component } from "react";
+import { CustomDna } from "../npmpackages/loaders/loaders";
+import { OrderList } from "../list/list";
+import { Image } from "../images/images";
 
-import { Component } from "react"
-import { CustomDna } from "../npmpackages/loaders/loaders"
-import { OrderList } from "../list/list"
-import { Image } from "../images/images"
+class ReceipeList extends Component {
+  state = {
+    Receipes: [],
+    loading: true,
+    error: false,
+  };
 
+  componentDidMount() {
+    console.log("fetching data here");
+    this.fetchData();
+  }
 
-class ReceipeList extends Component{
-     
-    state={
-        Receipes:[],
-        loading:true,
-        error:false
-    }
+  fetchData = async () => {
+    const data = await fetch("https://dummyjson.com/recipes");
+    const response = await data.json();
+    const { recipes } = response;
 
-     componentDidMount(){
-        console.log("fetching data here")
-         this.fetchData()
-     }
+    this.setState(
+      {
+        Receipes: recipes,
+        loading: false,
+      },
+      () => {}
+    );
+  };
 
-     fetchData= async ()=>{
-        const data= await fetch('https://dummyjson.com/recipes')
-        const response=await data.json()
-        const {recipes}=response
-         
+  render() {
+    return (
+      <>
+        <h1>Receipes Lists</h1>
 
-        this.setState({
-            Receipes:recipes,
-            loading:false
-        },()=>{})
-     }
+        {this.state.loading ? (
+          <CustomDna />
+        ) : (
+          <>
+            <h1>welcome to our restro</h1>
+            {this.state.Receipes.map((eachdata, index) => {
+              return (
+                <div key={index}>
+                  <h3>{eachdata.name}</h3>
 
+                  <OrderList list={eachdata.ingredients} />
 
-    render(){
-        return(
-            <>
-            <h1>Receipes Lists</h1>
-
-            {
-                this.state.loading?<CustomDna/>:<>
-                 <h1>welcome to our restro</h1>
-                 {
-                    this.state.Receipes.map((eachdata,index)=>{
-                        return (
-                           <div key={index}>
-                            <h3>{eachdata.name}</h3>
-                            
-                            <OrderList list={eachdata.ingredients}/>
-
-                            <Image source={eachdata.image} width={300} height={300}/>
-                           </div>
-                        )
-                    })
-                 }
-
-                </>
-            }
-            </>
-        )
-    }
+                  <Image source={eachdata.image} width={300} height={300} />
+                </div>
+              );
+            })}
+          </>
+        )}
+      </>
+    );
+  }
 }
 
-
 export default ReceipeList;
-
